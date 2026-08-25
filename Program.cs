44ca402250f3f9f1a59122a5861a8b26e8cca1f4
FelipeEvaldt-TI <felipe.evaldt@rede.ulbra.br>
2026-08-25 20:24:30 -0300

    Aula-04

--- Program.cs
+++ Program.cs
@@ -3,32 +3,38 @@ using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var jogos = new List<Jogo>
+{
+    new Jogo(1, "Hollow Knight", true),
+    new Jogo(2, "Grand Theft Auto 6", false)
+};
+
 app.MapGet("/", () => "API do catálogo de jogos está no ar!");
 
 app.MapGet("/api/jogos", () =>
 {
-    return Results.Ok(new[]
-    {
-        new{id = 1, titulo = "Hollow Knight", disponivel = true},
-        new{id = 2, titulo = "Grand Theft Auto 6", disponivel = false},
-    });
+    return Results.Ok(jogos);
 });
 
 app.MapGet("/api/jogos/{id:int}", (int id) => 
 {
-    if (id == 1) return Results.Ok(
-        new{id = 1, titulo = "Hollow Knight", disponivel = true});
-    if (id == 2) return Results.Ok(
-        new{id = 2, titulo = "Grand Theft Auto 6", disponivel = false});
-    return Results.NotFound(new {mensagem = "Jogo não encontrado!"});
+    var jogoEncontrado = jogos.Find(jogo => jogo.id == id);
+    if (jogoEncontrado is null)
+    {
+        return Results.NotFound();
+    };
+    return Results.Ok(jogoEncontrado);
 });
 
-app.MapPost("/api/jogos", async (HttpRequest request) =>
+app.MapPost("/api/jogos", (JogoDTO dados) =>
 {
-    // Lê o corpo da requisição como JSON
-    using JsonDocument documento = await JsonDocument.ParseAsync(request.Body);
-    string? tituloCriado = documento.RootElement.GetProperty("titulo").GetString();
-    return Results.Created("/api/jogos/3", new {id = 3, titulo = tituloCriado, disponivel = true});
+   int proximoId = jogos.Count + 1;
+   var novoJogo = new Jogo(proximoId, dados.titulo, true);
+   jogos.Add(novoJogo);
+   return Results.Ok(novoJogo);
 });
 
 app.Run();
+
+record Jogo(int id, string titulo, bool disponivel);
+record JogoDTO(string titulo);
